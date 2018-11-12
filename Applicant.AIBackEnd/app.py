02b843b0d30e60e69335359860bbb5abc875f92b
Matt Grant <mattgra@umich.edu>
2018-11-11 21:06:03 -0500

    Add files via upload
    Starter code for back end API

---
+++ Applicant.AIBackEnd/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
+import functions
+
+#Flask and MySQL setup
+application = app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+#MySQL setup
+'''
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'sdocs123'
+app.config['MYSQL_DB'] = 'AAIDB'
+app.config['MYSQL_HOST'] = 'aa5mho3pd0cv71.czzaljfmuz2x.us-east-2.rds.amazonaws.com'
+app.config['MYSQL_PORT'] = 3306
+'''
+
+mysql = MySQL()
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'sdocs'
+app.config['MYSQL_DB'] = 'AAIDB'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route('/CreateOrg', methods=['POST'])
+@cross_origin(origin='*')
+def post_create_org():
+	data = request.get_json()
+	cursor = mysql.connection.cursor()
+	cursor.execute("INSERT INTO Organizations ( name, description ) VALUES ( %s, %s )", [data['org_name'], data['description']])
+	mysql.connection.commit()
+	cursor.execute("SELECT * FROM Organizations WHERE name = %s", [data["org_name"]])
+	results = cursor.fetchall()
+	for row in results:
+		print(row[0])
+	return "Yeehaw\n"
+
+@app.route('/getOrganizationInfo', methods=['POST'])
+@cross_origin(origin='*')
+def get_hash():
+	data = request.form
+	#cursor = mysql.connection.cursor()
+	return None
+
+@app.route('/getOrganizationInfo', methods=['GET'])
+@cross_origin(origin='*')
+def true_get_hash():
+	return None
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ Applicant.AIBackEnd/tableGeneration.sql
@@ -0,0 +1,69 @@
+CREATE TABLE Organizations (
+	org_id BIGINT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(40) UNIQUE,
+	description VARCHAR(200),
+	PRIMARY KEY (org_id)
+);
+
+CREATE TABLE Postings (
+	post_id BIGINT NOT NULL AUTO_INCREMENT,
+	org_id BIGINT NOT NULL,
+	name VARCHAR(40),
+	status ENUM('OPEN', 'CLOSED'),
+	description VARCHAR(200),
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (org_id) REFERENCES Organizations(org_id)
+);
+
+CREATE TABLE Users (
+	user_id BIGINT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(40),
+	email VARCHAR(80) UNIQUE,
+	password VARCHAR(64),
+	PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Questions (
+	question_id BIGINT NOT NULL AUTO_INCREMENT,
+	post_id BIGINT NOT NULL,
+	question VARCHAR(400),
+	FOREIGN KEY (post_id) REFERENCES Postings(post_id),
+	PRIMARY KEY (question_id)
+);
+
+/*CREATE TABLE DropdownOption (
+	question_id BIGINT NOT NULL AUTO_INCREMENT,
+	post_id BIGINT NOT NULL,
+	dropdown_text VARCHAR(40),
+	FOREIGN KEY (post_id) REFERENCES Postings(post_id),
+	FORIEGN KEY (question_id) REFERENCES Questions(question_id),
+	PRIMARY KEY (post_id, question_id)
+);*/
+
+CREATE TABLE Answers (
+	user_id BIGINT NOT NULL,
+	post_id BIGINT NOT NULL,
+	question_id BIGINT NOT NULL,
+	answer VARCHAR(400),
+	FOREIGN KEY (question_id) REFERENCES Questions(question_id),
+	FOREIGN KEY (post_id) REFERENCES Postings(post_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	PRIMARY KEY (post_id, user_id)
+);
+
+
+CREATE TABLE Applicants (
+	user_id BIGINT NOT NULL,
+	post_id BIGINT NOT NULL,
+	FOREIGN KEY (post_id) REFERENCES Postings(post_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	PRIMARY KEY (post_id, user_id)
+);
+
+CREATE TABLE Members (
+	user_id BIGINT NOT NULL,
+	org_id BIGINT NOT NULL,
+	FOREIGN KEY (org_id) REFERENCES Organizations(org_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	PRIMARY KEY (org_id, user_id)
+);
