f1f511ad4853474592bc8e75edf0168248458dc1
thopel <thopel@umich.edu>
2018-11-14 11:03:17 -0500

    Add files via upload

--- testingScripts/user.sh
+++ testingScripts/user.sh
@@ -1,5 +1,5 @@
 curl --request POST -H "Content-Type: application/json" -d '{"name":"bob","email":"bob@bob.com","password":"bob!"}' http://127.0.0.1:5000/CreateUser
-curl --request POST -H "Content-Type: application/json" -d '{"name":"thom","email":"thom@b.com","password":"bob!"}' http://127.0.0.1:5000/CreateUser
+curl --request POST -H "Content-Type: application/json" -d '{"name":"thom","email":"thom@bob.com","password":"bob!"}' http://127.0.0.1:5000/CreateUser
 curl --request POST -H "Content-Type: application/json" -d '{"name":"matt","email":"matt@bob.com","password":"bob!"}' http://127.0.0.1:5000/CreateUser
 curl --request POST -H "Content-Type: application/json" -d '{"name":"jordan","email":"jordan@bob.com","password":"bob!"}' http://127.0.0.1:5000/CreateUser
 curl --request POST -H "Content-Type: application/json" -d '{"name":"alexis","email":"alexis@bob.com","password":"bob!"}' http://127.0.0.1:5000/CreateUser
