c2e51f68d5e59024ca1441519a5003abc1b5a091
Matt Grant <mattgra@umich.edu>
2018-11-14 15:43:41 -0500

    Update app.py

--- Applicant.AIBackEnd/app.py
+++ Applicant.AIBackEnd/app.py
@@ -3,9 +3,6 @@ from flask_mysqldb import MySQL
 from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 
-import os
-#import sh
-
 #Flask and MySQL setup
 application = app = Flask(__name__)
 api = Api(app)
@@ -29,10 +26,9 @@ app.config['MYSQL_PORT'] = 3306
 mysql = MySQL()
 # testing locally
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'mysql'
+app.config['MYSQL_PASSWORD'] = 'sdocs'
 app.config['MYSQL_DB'] = 'AAIDB'
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_PORT'] = 3306
 mysql.init_app(app)
 
 
@@ -61,7 +57,7 @@ def post_create_org():
 	if cursor.rowcount > 0:
 		return "ORG ALREADY EXISTS\n"
 
-	cursor.execute("INSERT INTO Organizations ( name, description ) VALUES ( %s, %s )", [data['org_name'], data['description']])
+	cursor.execute("INSERT INTO Organizations ( name, description, email ) VALUES ( %s, %s, %s )", [data['org_name'], data['description'], data['email']])
 	cursor.execute("SELECT * FROM Organizations WHERE name = %s", [data["org_name"]])
 	results = cursor.fetchall()
 	retId = 0
@@ -76,7 +72,10 @@ def post_create_org():
 	cursor.execute("INSERT INTO Members (user_id, org_id) VALUES ( %s, %s )", [results[0], retId])
 	mysql.connection.commit()
 
-	return "OrganizationID: " + str(retId) + "\n"
+	retIdData = {}
+	print("RetId: ", retId)
+	retIdData["OrganizationId"] = retId
+	return jsonify(retIdData), 200
 
 
 #curl --request POST -H "Content-Type: application/json" -d '{"name":"bob bob","email":"bob@bob.com","password":"bob!"}' http://127.0.0.1:5000/CreateUser
@@ -99,37 +98,12 @@ def post_create_user():
 		retId = row[0]
 	return "UserID: " + str(retId) + "\n"
 
-#curl --request POST -H "Content-Type: application/json" -d '{"org_name":"BobsClub","email":"bob@bob.dcom","pos_name":"Best Friend","answers":["need a friend", "im 6 feet tall", "seems like we would be a good fit", "123"],"answers_ML":[[7,2],[-1,-1],[8,5],[-2, 123]]}' http://127.0.0.1:5000/CreateSubmission
-
-# Answers_ML explanation:
-# The answers_ML value is a list of lists (more like list of tuples). The first number in each inner list represents the size of the list of choices in a dropdown field.
-# The second number represents the index of the choice that was chosen. If the field is a text field, set both numbers to -1. If the field is an integer field, set the 
-# first number to -2 and the second number to the integer value.
+#curl --request POST -H "Content-Type: application/json" -d '{"org_name":"BobsClub","email":"bob@bob.dcom","pos_name":"Best Friend","answers":["need a friend", "im 6 feet tall", "seems like we would be a good fit"]}' http://127.0.0.1:5000/CreateSubmission
 @app.route('/CreateSubmission', methods=['POST'])
 @cross_origin(origin='*')
 def post_create_submission():
 	data = request.get_json()
 	cursor = mysql.connection.cursor()
-
-	
-	if not os.path.isdir("mldata/"):
-		os.mkdir("mldata/")
-    
-	with open('mldata/run_data.txt', 'w') as f:
-		data_len = len(data['answers_ML'])
-		f.write(','+str(data_len))
-		for answer in data['answers_ML']:
-			code = answer[0]
-			val = answer[0]
-			if code != -1:
-				if code > 0:
-					options = [0] * code
-					options[val] = 1
-				elif code == -2:
-					options = [val]
-			for choice in options:
-				f.write(','+str(choice))    
-
 
 	#get User id:
 	cursor.execute("SELECT * FROM Users WHERE email = %s", [data["email"]])
@@ -218,7 +192,28 @@ def post_create_posting():
 @app.route('/getAllSubmissions', methods=['GET'])
 @cross_origin(origin='*')
 def get_submissions():
-	
+	'''
+		get all submissions for a given user
+		{
+			email // user_id only requirement
+
+		}
+
+		format sent back of type
+
+		{
+			user_id // email
+			[ 
+				{
+					post_id,
+					org_id,
+					status,
+				}
+			
+			]
+
+		}
+	'''
 
 	data = request.get_json()
 	cursor = mysql.connection.cursor()
@@ -230,45 +225,34 @@ def get_submissions():
 	user_id = cursor.fetchone()
 
 	return_data = {}
-	return_data['user_id'] = user_id
-	return_data['submissions'] = []
-	cursor.execute("SELECT a.post_id, a.status, p.org_id FROM applicants a, postings p WHERE a.user_id = %s \
-				    AND p.post_id = a.post_id", [user_id])
+	cursor.execute("SELECT post_id, org_id, status FROM applicants where user_id = %s", [user_id])
 	results = cursor.fetchall()
-
-	i = 0
-
-	# for every submission to a posting this user has made,
-	# provide the list of all questions/responses 
 	for row in results:
-		return_data['submissions'].append({})
-		return_data['submissions'][i]['post_id'] = row[0]
-		return_data['submissions'][i]['status'] = row[1]
-		return_data['submissions'][i]['org_id'] = row[2]
-
-		cursor.execute("SELECT q.question_id, q.question, a.answer FROM questions q, answers a \
-							WHERE q.question_id = a.question_id \
-							AND a.post_id = q.post_id \
-							AND a.post_id = %s \
-							AND a.user_id = %s", [row[0], user_id])
-		responses = cursor.fetchone()
-		return_data['submissions'][i]['responses'] = {}
-		return_data['submissions'][i]['responses']['question_id'] = responses[0]
-		return_data['submissions'][i]['responses']['question'] = responses[1]
-		return_data['submissions'][i]['responses']['answer'] = responses[2]
-		i += 1
+		print("temp")
 
 
-	return jsonify(return_data) 
+	return "submissions retrieved!" 
 
 @app.route('/updateApplicant', methods=['POST'])
 @cross_origin(origin='*')
 def update_applicant():
 	data = request.get_json()
 
+	'''
+		presumably the json format will be something along the lines of 
+		{
+			email // user_id
+			posting id
+			decision == 'INTERVIEW/ACCEPT/REJECT'
+
+
+		}
+
+	'''
+
 	cursor = mysql.connection.cursor()
 
-	if (data['status'] not in ['INTERVIEW', 'ACCEPT', 'REJECT', 'PENDING']):
+	if (data['status'] not in ['INTERVIEW', 'ACCEPT', 'REJECT']):
 		return "invalid status update"
 
 	cursor.execute("SELECT user_id FROM users where email = %s", [data['email']])
@@ -288,10 +272,8 @@ def update_applicant():
 		if (cursor.rowcount != 1):
 			return "Error invalid org"
 		orgs = cursor.fetchone()
-		cursor.execute("SELECT * FROM members where user_id = %s AND org_id = %s", [user_id, orgs])
-		if (cursor.rowcount == 0):
-			cursor.execute("INSERT INTO members ( user_id, org_id) VALUES (%s, %s)", [user_id, orgs])
 
+		cursor.execute("INSERT INTO members ( user_id, org_id) VALUES (%s, %s)", [user_id, data['post_id']])
 
 
 	mysql.connection.commit()
@@ -342,78 +324,111 @@ def get_all_org_info():
 			orgData["postings"].append(postData)
 		data["organizations"].append(orgData)
 	
+	return jsonify(data)
 
+@app.route('/getOrganizationInfoFromEmail', methods=['POST'])
+@cross_origin(origin='*')
+def get_all_org_info_by_email():
+	cursor = mysql.connection.cursor()
+	inData = request.get_json()
+	data = {
+		"organizations": [
 
-
-
-
-
-	dataTemp = {
-		"user" : {
-			"name" : "bob",
-			"id:" : 10,
-			"password" : "hhhh",
-			"email" : "i@bob.com"
-		},
-		"organizations" : [
-		{
-		 "name": "investing",
-		 "id" : 1,
-		 "members" : [ 
-			      { "id" : 1, "name" : "thomas" },
-					  { "id" : 2, "name" : "troy" }
-					],
-		 "postings" :[{
-			"id": 1,
-			"name" : "consultant",
-			"status" : "open",
-			"description" : "you will tell people how to invest things",
-			"questions" : [ { "question" : "why do you want to consult?", "type": "text", "answers" : [""] },
-				{ "question" : "but really, why?", "type": "text", "answers" : [""] },
-				{ "question" : "years experience?", "type": "dropdown", "answers" : ["some", "absolutely none"] } ]
-
-			,
-			"applicants" : [ {"id" : 3, "name" : "jordan", "answers" : [ "cause fun", "cause really fun", "some"] }]
-
-		}]},
-
-		{
-		 "name": "frat",
-		 "id" : 2,
-		 "members" : [ 
-			      { "id" : 3, "name" : "jordan" },
-					  { "id" : 2, "name" : "troy" }
-					],
-		 "postings" :[{
-			"id": 1,
-			"name" : "frat fellow",
-			"status" : "closed",
-			"description" : "you will do frat things",
-			"questions" : [ { "question" : "why this frat?", "type": "text", "answers" : [""] } ],
-			"applicants" : [ {"id" : 4, "name" : "alexis", "answers" : [ "cause"] },
-							 {"id" : 5, "name" : "matt", "answers" : ["because its my frat"]}]
-
-		}]},
-
-		{
-		 "name": "lonelyville",
-		 "id" : 2,
-		 "members" : [ 
-			      { "id" : 6, "name" : "collin" }
-					],
-		 "postings" :[{
-			"id": 1,
-			"name" : "come say hello",
-			"status" : "open",
-			"description" : "be a friend",
-			"questions" : [ ],
-			"applicants" : []
-
-		}]}
 		]
 	}
+	
+	cursor.execute("SELECT * FROM Organizations WHERE email = %s", [inData['email']])
+	results = cursor.fetchall()
+	for row in results:
+		print(row)
+		orgData = {}
+		orgData["name"] = row[1]
+		orgData["id"] = row[0]
+		orgData["members"] = [ 
+			      { "id" : 1, "name" : "thomas" },
+					  { "id" : 2, "name" : "troy" }
+					]
+		orgData["postings"] = []
+		cursor.execute("SELECT * FROM Postings WHERE org_id = %s", [row[0]])
+		innerResults = cursor.fetchall()
+		for innerRow in innerResults:
+			postData = {}
+			postData["id"] = innerRow[0]
+			postData["name"] = innerRow[2]
+			postData["status"] = innerRow[3]
+			postData["description"] = innerRow[4]
+			postData["questions"] = []
+			cursor.execute("SELECT * FROM Questions WHERE post_id = %s", [innerRow[0]])
+			questionResults = cursor.fetchall()
+			for questionRow in questionResults:
+				postData["questions"].append(questionRow[2])
+			orgData["postings"].append(postData)
+		data["organizations"].append(orgData)
+	
 	return jsonify(data)
+
+
+
+
+@app.route('/getApplicantsFromPosting', methods=['POST'])
+@cross_origin(origin='*')
+def get_applicant_from_posting():
+	data = request.get_json()
+	cursor = mysql.connection.cursor()
+	
+	#get org id
+	cursor.execute("SELECT * FROM Organizations WHERE name = %s", [data["org_name"]])
+	results = cursor.fetchall()
+	orgId = -1
+	for row in results:
+		orgId = row[0]
+
+	if orgId == -1:
+		return "ORG NOT FOUND\n"
+
+	#get post id
+	cursor.execute("SELECT * FROM Postings WHERE org_id = %s AND name = %s", [orgId, data['pos_name']])
+	results = cursor.fetchall()
+	postId = -1	
+	for row in results:
+		postId = row[0]
+
+	if postId == -1:
+		return "POST NOT FOUND\n"
+	
+	
+	cursor.execute("SELECT * FROM Applicants WHERE post_id = %s", [postId])
+	results = cursor.fetchall()
+	retData = { "applicants": [] }
+	for applicant in results:
+		userId = applicant[0]
+		status = applicant[2]
+		applicantData = { "status":status }
+		
+		cursor.execute("SELECT * FROM Users WHERE user_id = %s", [userId])
+		userResults = cursor.fetchall()
+		userData = {}
+		for ur in userResults:
+			userData["id"] = userId
+			userData["name"] = ur[1]
+			userData["email"] = ur[2]
+		applicantData["userInfo"] = userData
+		
+		cursor.execute("SELECT * FROM Answers WHERE user_id = %s AND post_id = %s", [userId, postId])
+		answerResults = cursor.fetchall()
+		applicantData["questions"] = []
+		for ar in answerResults:
+			questionId = ar[2]
+			answer = ar[3]
+			question = ""
+			cursor.execute("SELECT * FROM Questions WHERE question_id = %s", [questionId])
+			questionResults = cursor.fetchall()
+			for qr in questionResults:
+				question = qr[2]
+			applicantData["questions"].append({"question":question, "answer":answer})
+		retData["applicants"].append(applicantData)
+	return jsonify(retData)
+		
 
 if __name__ == '__main__':
 	app.run(debug=True)
-
